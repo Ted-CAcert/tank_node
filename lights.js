0d217fd92f8e4c3abc75e21bc344bb498361f057
Bernhard "Ted" Fröhlich <ted@convey.de>
2023-02-11 20:57:32 +0100

    Blinking

--- lights.js
+++ lights.js
@@ -5,6 +5,9 @@ const channel = ws281x(6, { gpio: 12, stripType: 'ws2812' });
 const colorsArray = channel.array;
 
 let lightTimeout;
+let blinkInterval;
+let isOn=false;
+let BlinkState = 0;
 
 /*  color is an array of RGB values, one for each LED 
     duration is in minutes, -1 for "no timeout"
@@ -27,14 +30,40 @@ exports.on=function (duration, color) {
         lightTimeout = setTimeout(this.off, duration*60000, this);
     }
     cam.snap();
+
+    isOn=true;
 }
 
 exports.off=function (obj) {
     if (lightTimeout) clearTimeout(lightTimeout);
-  
+
+    BlinkState = 0;
     if (obj) {
       obj.on(-1, [ [1,0,0], [0,0,0], [0,0,0], [0,1,0], [0,0,0], [0,0,0] ]);
     } else {
       this.on(-1, [ [1,0,0], [0,0,0], [0,0,0], [0,1,0], [0,0,0], [0,0,0] ]);
     }
+    isOn=false;
 }
+
+function timerBlink(obj) {
+    if (isOn) return;
+
+    if (BlinkState == 0) {
+        if (obj) {
+            obj.on(-1, [ [1,0,0], [0,0,0], [0,0,0], [0,0,0], [0,0,0], [0,0,0] ]);
+        } else {
+            this.on(-1, [ [1,0,0], [0,0,0], [0,0,0], [0,0,0], [0,0,0], [0,0,0] ]);
+        }
+        BlinkState=1;
+    } else {
+        if (obj) {
+            obj.on(-1, [ [0,0,0], [0,0,0], [0,0,0], [0,1,0], [0,0,0], [0,0,0] ]);
+        } else {
+            this.on(-1, [ [0,0,0], [0,0,0], [0,0,0], [0,1,0], [0,0,0], [0,0,0] ]);
+        }
+        BlinkState=0;
+    }
+}
+
+blinkInterval = setInterval(timerBlink, 1000, null);
