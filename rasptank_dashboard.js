a07beb5dae48f807277cd35a5d84b121a1b98a66
Bernhard Fröhlich <ted@convey.de>
2023-02-12 23:05:23 +0100

    progress...

--- rasptank_dashboard.js
+++ rasptank_dashboard.js
@@ -1,6 +1,7 @@
 var xhttp;
 var obSnapshotImg;
 var obEditAddr;
+var etagImg;
 var intervalWork;
 
 function main() {
@@ -10,10 +11,54 @@ function main() {
 
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-           console.log("Done");
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                const bytes = new Uint8Array(this.response);
+                var len = bytes.byteLength;
+                var binary = '';
+                for (var i = 0; i < len; i++) {
+                    binary += String.fromCharCode( bytes[ i ] );
+                }
+                var tmpString = "data:image/jpg;base64,"+btoa(binary);
+                obSnapshotImg.src=tmpString;
+
+                etagImg = xhttp.getResponseHeader("Etag");
+            } else if (this.status == 304) {
+//                console.log("Not modified");
+            } else {
+                console.log("Unexpected result: "+this.status+" "+this.statusText);
+            }
+            xhttp = undefined;
         }
     };
+    xhttp.onprogress = function(event) {
+        if (event.total > 0) {
+            console.log("Progress "+event.loaded+"/"+event.total);
+        }
+    }
+    xhttp.open("GET", "../data/image.jpg", true);
+    xhttp.responseType="arraybuffer";
+    if (etagImg) {
+        xhttp.setRequestHeader("If-None-Match", etagImg);
+    }
+    xhttp.send();
 }
+
+// convert a Unicode string to a string in which
+// each 16-bit unit occupies only one byte
+function toBinary(string) {
+  const codeUnits = Uint16Array.from(
+    { length: string.length },
+    (element, index) => string.charCodeAt(index)
+  );
+  const charCodes = new Uint8Array(codeUnits.buffer);
+
+  let result = "";
+  charCodes.forEach((char) => {
+    result += String.fromCharCode(char);
+  });
+  return result;
+}
+
 
 intervalWork=setInterval(main, 1000);
